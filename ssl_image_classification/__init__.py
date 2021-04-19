e579cedb07a61347de9344bf6c797ccb537f89db
JanMarcelKezmann <jankezmann@t-online.de>
2021-04-19 07:44:54 +0200

    Make imports easier via __init__.py

--- ssl_image_classification/__init__.py
+++ ssl_image_classification/__init__.py
@@ -1,3 +1,5 @@
 from .__version__ import __version__
 
-from .main import main, get_arg_parser
+from .main import main, get_arg_parser, train, validate
+from .libml import data_augmentations, models, optimizers, preprocess, train_utils, train_utils
+from .algorithms import fixmatch, ict, meanteacher, mixmatch, mixup, pimodel, pseudolabel, remixmatch, vat
